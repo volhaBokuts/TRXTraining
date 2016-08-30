e0e569b50a43322d6ac72f57359e95b15277771f
Fellow <bokut.olya@gmail.com>
2016-08-30 13:28:24 +0300

    change splash screen picture

--- app/src/main/java/com/example/fellow/trxtraining/SplashScreenActivity.java
+++ app/src/main/java/com/example/fellow/trxtraining/SplashScreenActivity.java
@@ -1,13 +1,28 @@
 package com.example.fellow.trxtraining;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    //Время в милисекундах, в течение которого будет отображаться splash screen
+    private final int SPLASH_SCREEN_TIME = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                SplashScreenActivity.this.startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_SCREEN_TIME);
+
     }
 }
