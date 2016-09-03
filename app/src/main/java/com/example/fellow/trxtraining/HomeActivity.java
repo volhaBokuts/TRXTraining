b2eb000effa81b00f48f453e12d4805607bd0763
agronin.al <agronin.al@gmail.com>
2016-09-03 13:21:34 +0300

    add activities: ExercisesActivity, PlaygroundActivity, UserComplexActivity

--- app/src/main/java/com/example/fellow/trxtraining/HomeActivity.java
+++ app/src/main/java/com/example/fellow/trxtraining/HomeActivity.java
@@ -1,13 +1,41 @@
 package com.example.fellow.trxtraining;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnHome, btnExercises, btnPlaygrounds, btnUserComplexes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHome = (Button) findViewById(R.id.buttonHome);
+        btnExercises = (Button) findViewById(R.id.buttonExercises);
+        btnUserComplexes = (Button) findViewById(R.id.buttonUserComplexes);
+        btnPlaygrounds = (Button) findViewById(R.id.buttonPlaygrounds);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.buttonExercises:
+                Intent intentExercises = new Intent(this, ExercisesActivity.class);
+                startActivity(intentExercises);
+                break;
+            case R.id.buttonUserComplexes:
+                Intent intentUserComplexes = new Intent(this, UserComplexesActivity.class);
+                startActivity(intentUserComplexes);
+                break;
+            case R.id.buttonPlaygrounds:
+                Intent intentPlaygrounds = new Intent(this, PlaygroundsActivity.class);
+                startActivity(intentPlaygrounds);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/fellow/trxtraining/UserComplexesActivity.java
@@ -0,0 +1,13 @@
+package com.example.fellow.trxtraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserComplexesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_complexes);
+    }
+}
